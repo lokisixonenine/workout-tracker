4c26d718b7df8626f8e187cb5e9b8df86de87444
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-04-24 14:07:57 -0700

    created api

---
+++ public/api.js
@@ -0,0 +1,42 @@
+const API = {
+    async getPreviousWorkout() {
+        let res;
+        try {
+            res = await fetch("/api/workouts");
+        } catch (err) {
+            console.log(err);
+        }
+        const json = await res.json();
+        return json[json.length - 1];
+    },
+
+    async addNewExercise(data) {
+        const id = location.search.split("=")[1];
+
+        const res = await fetch("/api/workouts" + id, {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        });
+
+        const json = await res.json();
+        return json;
+    },
+    
+    async createWorkout(data = {}) {
+        const res = await fetch("/api/workouts", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {"Content-Type": "application/json"}
+        });
+
+        const json = await res.json();
+        return json;
+    },
+
+    async getWorkoutsWithinRange() {
+        const res = await fetch(`/api/workouts/range`);
+        const json = await res.json();
+        return json;
+    },
+};
