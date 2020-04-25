0a7a532973406daf7163fb76b0c8a0750594a4ed
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-04-25 08:47:58 -0700

    Recoded server.js

--- server.js
+++ server.js
@@ -1,90 +1,31 @@
-require('dotenv').config()
+// initialize dependencies
 const express = require("express");
-const logger = require("morgan");
-const mongojs = require('mongojs')
 const mongoose = require("mongoose");
-const path = require('path')
+const logger = require("morgan");
 
+// initialize PORT
 const PORT = process.env.PORT || 3000;
 
-const User = require("./models/models");
+// set up and call dependencies
 const app = express();
-app.use(logger("dev"));
 
+app.use(logger("dve"));
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhoust/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
+// set up routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
-// file routes
-app.get("/", (_req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
-
-app.get("/exercise", (_req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
-})
-
-// api/workouts
-app.get("/api/workouts", (_, res) => {
-    User.find()
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-app.post("/api/workouts", (_, res) => {
-    User.create({})
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-app.post("/api/workouts/:id", ({
-    body,
-    params
-}, res) => {
-    User.findByIdAndUpdate(params.id, {
-            $push: {
-                exercises: body
-            }
-        }, {
-            new: true,
-            runValidators: true
-        })
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-// api/workouts/range 
-app.get('/api/workouts/range', (_, res) => {
-    User.find()
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-
+// port listener
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`Go workout! App is now running on port ${PORT}!`);
 });
